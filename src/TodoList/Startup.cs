25765e7e07070c407a66d56b4d0d22d6b4658233
balachir <balachirt@outlook.com>
2014-12-05 14:10:09 -0800

    Create TodoList app with a default home page styled with Bootstrap

---
+++ src/TodoList/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace TodoList
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+        }
+
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // Add static files to the request pipeline.
+            app.UseStaticFiles();
+
+            app.UseMvc();
+        }
+    }
+}
